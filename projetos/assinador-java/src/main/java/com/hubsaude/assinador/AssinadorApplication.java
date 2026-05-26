0b2b660cb919ae7cd1942dff1d06f317759eb3fa
danilo-sgalvao <danilo.galvao@discente.ufg.br>
2026-05-26 14:44:49 -0300

    feat: dispatcher reserva o modo 'serve' (ainda nao implementado)

--- projetos/assinador-java/src/main/java/com/hubsaude/assinador/AssinadorApplication.java
+++ projetos/assinador-java/src/main/java/com/hubsaude/assinador/AssinadorApplication.java
@@ -11,6 +11,13 @@ import com.hubsaude.assinador.presentation.cli.CliRunner;
 public class AssinadorApplication {
 
     public static void main(String[] args) {
+        if (args.length > 0 && "serve".equals(args[0])) {
+            // Ponto de extensão da Sprint 3 (US-02.4): modo servidor HTTP via Spring Boot.
+            System.err.println("Modo servidor (serve) ainda não implementado.");
+            System.exit(1);
+            return;
+        }
+
         SignatureService service         = new FakeSignatureService();
         RequestValidator validator       = new RequestValidator();
         SignUseCase      signUseCase     = new SignUseCase(service, validator);
